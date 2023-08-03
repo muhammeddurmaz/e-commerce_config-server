58f8a088f6e50fc418336765d5427980f5410dad
muhammeddurmaz <muhammed.durmaz@derinarge.com>
2023-08-03 16:08:38 +0300

    start config server

---
+++ src/test/java/com/durmaz/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.durmaz.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
